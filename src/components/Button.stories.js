33f5f1423e9a9a0e3ba93095a34e287dea0a02d9
moonyerim2 <moonyerim2@gmail.com>
2023-02-06 17:37:32 +0900

    feat: Button 컴포넌트 스토리 추가

--- src/components/Button.stories.js
+++ src/components/Button.stories.js
@@ -4,9 +4,28 @@ import Button from "./Button";
 
 export default {
   component: Button,
-  title: "Button",
+  title: "button/Button",
 };
 
 const Template = (args) => <Button {...args}>버튼을 클릭하세요.</Button>;
 
-export const Default = Template.bind({});
+export const Primary = Template.bind({});
+Primary.args = {
+  size: "large",
+  color: "primary",
+  shape: "default",
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  size: "large",
+  color: "secondary",
+  shape: "default",
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  size: "large",
+  color: "disabled",
+  shape: "default",
+};
