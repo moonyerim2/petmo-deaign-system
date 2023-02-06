01dbb2b409dc98ff5ade3513a0ec4fd30e519fb9
moonyerim2 <moonyerim2@gmail.com>
2023-02-06 17:38:21 +0900

    feat: IconTextButton 컴포넌트 스토리 추가

---
+++ src/components/IconTextButton.stories.js
@@ -0,0 +1,30 @@
+import React from "react";
+import IconTextButton from "./IconTextButton";
+import CheckIcon from "../assets/CheckIcon";
+
+export default {
+  component: IconTextButton,
+  title: "button/IconTextButton",
+};
+
+const Template = (args) => (
+  <IconTextButton {...args}>버튼을 클릭하세요.</IconTextButton>
+);
+
+export const Left = Template.bind({});
+Left.args = {
+  size: "large",
+  color: "primary",
+  shape: "default",
+  Icon: CheckIcon,
+  iconLocation: "left",
+};
+
+export const Right = Template.bind({});
+Right.args = {
+  size: "large",
+  color: "primary",
+  shape: "default",
+  Icon: CheckIcon,
+  iconLocation: "right",
+};
