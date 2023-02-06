937da29f48ce043d9421538dd0241ab72f631f9c
moonyerim2 <moonyerim2@gmail.com>
2023-02-06 23:03:31 +0900

    feat: Textarea 컴포넌트 스토리 추가

---
+++ src/components/Textarea.stories.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Textarea from "./Textarea";
+
+export default {
+  component: Textarea,
+  title: "Textarea/Textarea",
+};
+
+const TypedTemplate = (args) => (
+  <Textarea placeholder="텍스트를 입력해주세요." {...args}>
+    청춘은 위하여 천하를 그리하였는가?
+  </Textarea>
+);
+
+const UnTypedTemplate = (args) => (
+  <Textarea placeholder="텍스트를 입력해주세요." {...args}></Textarea>
+);
+
+export const Typed = TypedTemplate.bind({});
+
+export const Placeholder = UnTypedTemplate.bind({});
