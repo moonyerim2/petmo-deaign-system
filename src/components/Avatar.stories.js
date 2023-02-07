127e4cde2e67d7a23d5feb0a8e9d33770ae826bc
moonyerim2 <moonyerim2@gmail.com>
2023-02-07 14:41:43 +0900

    feat: Avatar 컴포넌트 스토리 추가

---
+++ src/components/Avatar.stories.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Avatar from "./Avatar";
+
+export default {
+  component: Avatar,
+  title: "avatar/Avatar",
+};
+
+const Template = (args) => <Avatar {...args} />;
+
+export const Small = Template.bind({});
+Small.args = {
+  size: "small",
+  imgSrc: require("../assets/avatar_no_img.png"),
+};
+
+export const Medium = Template.bind({});
+Medium.args = {
+  size: "medium",
+  imgSrc: require("../assets/avatar_no_img.png"),
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: "large",
+  imgSrc: require("../assets/avatar_no_img.png"),
+};
+
+export const LargeWithButton = Template.bind({});
+LargeWithButton.args = {
+  size: "large",
+  imgSrc: require("../assets/avatar_no_img.png"),
+  withButton: true,
+};
