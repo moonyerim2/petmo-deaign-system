138951e23fab157fb1a62c6da196a936e09427d5
moonyerim2 <moonyerim2@gmail.com>
2023-02-08 14:07:41 +0900

    feat: UserBadge 컴포넌트 스토리 추가

---
+++ src/components/UserBadge.stories.js
@@ -0,0 +1,44 @@
+import React from "react";
+import UserBadge from "./UserBadge";
+
+export default {
+  component: UserBadge,
+  title: "UserBadge/UserBadge",
+};
+
+const TypedTemplate = (args) => <UserBadge {...args} />;
+
+export const Small = TypedTemplate.bind({});
+Small.args = {
+  size: "small",
+  imgSrc: require("../assets/avatar_no_img.png"),
+  userName: "모찌",
+  address: "송파동",
+  elapsedTime: "2분전",
+};
+
+export const Medium = TypedTemplate.bind({});
+Medium.args = {
+  size: "medium",
+  imgSrc: require("../assets/avatar_no_img.png"),
+  userName: "모찌",
+  address: "송파동",
+  elapsedTime: "2분전",
+};
+
+export const Large = TypedTemplate.bind({});
+Large.args = {
+  size: "large",
+  imgSrc: require("../assets/avatar_no_img.png"),
+  userName: "모찌",
+  address: "송파동",
+};
+
+export const LargeWithButton = TypedTemplate.bind({});
+LargeWithButton.args = {
+  size: "large",
+  imgSrc: require("../assets/avatar_no_img.png"),
+  userName: "모찌",
+  address: "송파동",
+  withButton: true,
+};
