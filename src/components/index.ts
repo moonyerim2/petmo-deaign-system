06e16e81ae0e1d3ad3e810cc7f9bf10da07c46b7
moonyerim2 <moonyerim2@gmail.com>
2023-02-16 18:33:38 +0900

    feat: LargeCard export 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Button } from "./Button";
 export { default as IconTextButton } from "./IconTextButton";
 export { default as IconButton } from "./IconButton";
 export { default as Textarea } from "./Textarea";
+export { default as LargeCard } from "./LargeCard";
