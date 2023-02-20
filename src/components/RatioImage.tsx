ff921352d3bd4e9c1c82d49fc014630d7ecb41a6
moonyerim2 <moonyerim2@gmail.com>
2023-02-20 21:17:59 +0900

    fix: LargeCard 이미지 부모 100% 차지하도록 수정

--- src/components/RatioImage.tsx
+++ src/components/RatioImage.tsx
@@ -16,8 +16,8 @@ const Image = styled.img`
   position: absolute;
   top: 0;
   left: 0;
-  width: 100;
-  height: 100;
+  width: 100%;
+  height: 100%;
 `;
 
 function RatioImage({ src, alt }: RatioImageProps) {
