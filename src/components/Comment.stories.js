8213037be3257f66618488db91ea9122fb68ab69
moonyerim2 <moonyerim2@gmail.com>
2023-02-27 15:30:39 +0900

    feat: 내가 쓴 댓글일 경우 스토리 추가

--- src/components/Comment.stories.js
+++ src/components/Comment.stories.js
@@ -31,3 +31,16 @@ SubComment.args = {
   content: "안녕하세요~",
   depth: 2,
 };
+
+export const MyComment = Template.bind({});
+MyComment.args = {
+  isMyComment: true,
+  userBadgeData: {
+    imgSrc: require("../assets/avatar_no_img.png"),
+    userName: "모찌모찌",
+    address: "송파동",
+    elapsedTime: "2분전",
+  },
+  content: "안녕하세요~",
+  depth: 2,
+};
